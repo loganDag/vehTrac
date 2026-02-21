e7d5b0025fac0839241dfd55cdfa320d7db57f2d
loganDag <logandag11@gmail.com>
2026-02-20 22:59:50 -0600

    Finished GetUserInfo conversion, closing issue 24

--- BackPhp/GetUserInfo.php
+++ BackPhp/GetUserInfo.php
@@ -1,8 +1,10 @@
 <?php
 $UserID_Cookie = $_SESSION["user_id"];
-$UserInfoQuery = "SELECT * FROM user_info WHERE user_uid=('$UserID_Cookie')";
-$GetInfoQuery= $conn->query($UserInfoQuery);
-while($UserInfo = mysqli_fetch_array($GetInfoQuery)){
+$UserInfoQuery = $conn->prepare("SELECT * FROM user_info WHERE user_uid= ?");
+$UserInfoQuery->bind_param("s", $UserID_Cookie);
+$UserInfoQuery->execute();
+$GetInfoQuery= $UserInfoQuery->get_result();
+while($UserInfo = $GetInfoQuery->fetch_assoc()){
    $user_first_name = $UserInfo["FName"];
    $user_app_id = $UserInfo["user_uid"];
 }
