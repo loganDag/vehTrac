ee5ed6cfad1ae17f4e3718ef6a9d803714ff5870
loganDag <logandag11@gmail.com>
2026-02-27 04:24:01 -0600

    Some minor changes to add later on.

--- ui/fuel/index.php
+++ ui/fuel/index.php
@@ -1,16 +1,18 @@
 <?php
 $DocRoot = $_SERVER["DOCUMENT_ROOT"];
-require("$DocRoot/../bootstrap.html");
 require("$DocRoot/includes/header.php");
 require("$DocRoot/includes/cookieCheck.php");
 require("$DocRoot/includes/menu.html");
 ?>
 <html>
+    <head>
+        <title>VehTrac | Fuel Log</title>
+    </head>
     <body data-bs-theme="<?php echo $theme;?>">
-    <div class="main_site_content">
+    <div class="main_site_content container min-vh-100 d-flex flex-column">
 
 
-
+</div>
 </body>
-<?php require("includes/footer.html");?>
+<?php require("$DocRoot/includes/footer.html");?>
 </html>
