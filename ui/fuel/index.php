c7541214d588059377e13ae3812a4f43bd6146a9
loganDag <logandag11@gmail.com>
2026-03-06 00:31:40 -0600

    Just added a bit more

--- ui/fuel/index.php
+++ ui/fuel/index.php
@@ -14,15 +14,17 @@ $UserCookie = $_SESSION["user_id"];
     <body data-bs-theme="<?php echo $theme;?>">
     <div class="main_site_content container min-vh-100 d-flex flex-column">
 
-<div class="container-fluid d-flex veh_main_con">
-                 <table class="table table-responsive table-hover caption-top table-striped-columns veh_table">
+<div class="container-fluid d-flex mx-auto">
+                 <table class="table table-responsive table-hover caption-top table-striped-columns fuel_table">
+                    <caption>List of all your fuel logs in the system:</caption>
                         <thead>
                                 <tr>
                             <th scope="col">Logged Miles</th>
                             <th scope="col">Date Logged</th>
-                            <th scope="col">Reason</th>
                             <th scope="col">Vehicle UID </th>
                             <th scope="col">Vehicle Nickname</th>
+                            <th scope="col">Reason</th>
+                            <th scope="col">Edit Log</th>
                                 </tr>
                         </thead>
                             <tbody>
@@ -34,10 +36,15 @@ $UserCookie = $_SESSION["user_id"];
                         $result = $stmt->get_result();
                         if ($result->num_rows > 0){
                                 while($Fuel_results = $result->fetch_assoc()){
-                        $dis_veh_make = $Fuel_results["miles"];
-                        $dis_veh_model = $Fuel_results["date"];
-                        $dis_veh_year = $Fuel_results["reason"];
+                        $dis_fuel_miles = $Fuel_results["miles"];
+                        $dis_db_time = $Fuel_results["date"];
+                        $dis_reason = $Fuel_results["reason"];
                         $dis_veh_uid = $Fuel_results["veh_uid"];
+                        $dis_db_id = $Fuel_results["db_id"];
+
+                        $dateTimeObj = new DateTime($dis_db_time);
+
+                        $human_readable_time = $dateTimeObj->format('F j, Y \\a\\t g:i a');
 
                           $CarIDGrabInfoSql = $conn->prepare("SELECT veh_uid, nickname FROM vehicles WHERE veh_uid=?");
                        $CarIDGrabInfoSql->bind_param("i", $dis_veh_uid);
@@ -54,11 +61,12 @@ $UserCookie = $_SESSION["user_id"];
                            }
                     
                         echo "<tr>";
-                        echo "<td>$dis_veh_make</td>";
-                        echo " <td>$dis_veh_model</td>";
-                        echo " <td>$dis_veh_year</td>";
+                        echo "<td>$dis_fuel_miles</td>";
+                        echo " <td>$human_readable_time</td>";
                         echo "<td> $veh_id </td>";
                         echo "<td>$veh_nickname_clean</td>";
+                        echo " <td>$dis_reason</td>";
+                        echo "<td><a href='https://vehtrac.logandag.dev/ui/fuel/editFuel.php?dbid=$dis_db_id'>Edit $dis_db_id</a></td>";
                         echo "</tr>";
                                 }
                     } 
