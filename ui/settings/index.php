aa2a6d501b841bd18a1e4d67841806d7f444ab4e
loganDag <logandag11@gmail.com>
2026-04-27 02:48:17 -0500

    Slowly adding a change username feature.

--- ui/settings/index.php
+++ ui/settings/index.php
@@ -7,7 +7,13 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 $UserID = $_SESSION["user_id"];
-
+     $stmt = $conn->prepare("SELECT * FROM user_info WHERE user_uid=?");
+     $stmt->bind_param("s", $UserID);
+     $stmt->execute();
+    $UserInfoResult = $stmt->get_result();
+    while ($UserInfo = $UserInfoResult->fetch_assoc()){
+            $UserInfoUsername = $UserInfo["username"];
+   }
 ?>
 <html>
     <head>
@@ -20,7 +26,34 @@ $UserID = $_SESSION["user_id"];
         <div class="text-center">
         <h2 class="jumbotron">Account Settings:</h2>
         </div>
-<div class="theme-switch d-flex flex-column align-items-center text-center">
+
+        <a name="sessions"></a>
+        <?php
+        require ("$DocRoot/BackPhp/DisplaySessions.php");
+          if (!isset($UserInfoUsername)){
+            $UserInfoUsernameShow = "None set";
+            $Disabled = "";
+          }else{
+            $UserInfoUsernameShow = $UserInfoUsername;
+            $Disabled = "disabled";
+          }
+        ?>
+         <div class="username_form ">
+            <form action="" method="post" class="d-flex flex-column align-items-center mt-3">
+                <div class="form-floating mb-3 w-auto">
+                <input type = "text" value = "<?php echo $UserInfoUsernameShow; ?>" id='username' name='username' <?php echo $Disabled; ?> class="form-control">
+                <label for="username">Username:</label>
+                <br>
+                <button class="w-auto btn btn-lg btn-primary mb-3" type="submit" name="submit_username" <?php echo $Disabled; ?>>Submit Username</button>
+                </div>
+            </form>
+            
+        </div>
+
+        <div class="reset_password">
+
+        </div>
+        <div class="theme-switch d-flex flex-column align-items-center text-center">
     <p class="fs-5">Display Theme:</p>
     <span class="fs-6 text-muted">Choose a theme to sync across your account for devices.</span>
 
@@ -39,14 +72,6 @@ $UserID = $_SESSION["user_id"];
         <button type="submit" class="btn btn-primary" name="theme_submit">Save theme choice</button>
     </form>
 </div>
-
-        <a name="sessions"></a>
-        <?php
-        require ("$DocRoot/BackPhp/DisplaySessions.php");
-        ?>
-        <div class="reset_password">
-
-        </div>
     </div> <!--END MAIN SITE CONTENT DIV-->
 </body>
 <?php require("$DocRoot/includes/footer.html"); ?>
