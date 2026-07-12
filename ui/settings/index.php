ea80764e1ae02471727f4e11e38994a1b2ec2c84
loganDag <logandag11@gmail.com>
2026-07-12 03:53:44 -0500

    Numerous updates and changes to close #13

--- ui/settings/index.php
+++ ui/settings/index.php
@@ -3,105 +3,234 @@ $DocRoot = $_SERVER["DOCUMENT_ROOT"];
 require("$DocRoot/includes/header.php");
 require "$DocRoot/includes/cookieCheck.php";
 require("$DocRoot/includes/menu.html");
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
-$CookieTheme = $_COOKIE['SiteTheme']; 
+$CookieTheme = $_COOKIE['SiteTheme'];
 $ErrorMessage = [];
 $UserTheme =  "";
 $UserID = $_SESSION["user_id"];
-     $stmt = $conn->prepare("SELECT * FROM user_info WHERE user_uid=?");
-     $stmt->bind_param("s", $UserID);
-     $stmt->execute();
-    $UserInfoResult = $stmt->get_result();
-    while ($UserInfo = $UserInfoResult->fetch_assoc()){
-            $UserInfoUsername = $UserInfo["username"];
-   }
+$stmt = $conn->prepare("SELECT * FROM user_info WHERE user_uid=?");
+$stmt->bind_param("s", $UserID);
+$stmt->execute();
+$UserInfoResult = $stmt->get_result();
+while ($UserInfo = $UserInfoResult->fetch_assoc()) {
+    $UserInfoUsername = $UserInfo["username"];
+    $UserInfoRank = $UserInfo["site_rank"];
+    $UserInfoEmail = $UserInfo["email"];
+}
 
-   if (isset($_POST["theme_submit"])){
-            $theme = $_POST['theme_choice'] ?? 'light';
-            $stmt = $conn->prepare('UPDATE user_info SET theme = ? WHERE user_uid = ?');
-            $stmt->bind_param('ss', $theme, $UserID);
-            $result = $stmt->execute();
-            if ($result){
-                $ErrorMessage[] = 'Saved applied theme! This will take effect on your next login.';
+if (isset($_POST["theme_submit"])) {
+    $theme = $_POST['theme_choice'] ?? 'light';
+    $stmt = $conn->prepare('UPDATE user_info SET theme = ? WHERE user_uid = ?');
+    $stmt->bind_param('ss', $theme, $UserID);
+    $result = $stmt->execute();
+    if ($result) {
+        $ErrorMessage[] = 'Saved applied theme! This will take effect on your next login.';
+    } else {
+        $ErrorMessage[] = "We were unable to save your theme, please try again!";
+    }
+}
+
+if ($UserInfoRank != '3'){
+    $PassDisable = "";
+}else{
+    $PassDisable = "disabled";
+}
+
+
+if (isset($_POST["submit_new_email"])){
+    $NewEmail = $_POST["new_email"];
+    $reset_email_input = $_POST["reset_email_confirm"];
+    $NewEmailLow = strtolower($NewEmail);
+    $ResetEmailLow = strtolower($reset_email_input);
+
+    if ($NewEmailLow == $ResetEmailLow){
+        $CurrentTime = new DateTime();
+
+        $interval = new DateInterval('PT15M');
+
+        $expirationTime = $CurrentTime->add($interval);
+
+        $expireTime = $expirationTime->format('Y-m-d H:i:s');
+
+        $reset_email_code = random_int(100000, 999999);
+
+        $stmt = $conn->prepare(
+            "UPDATE user_info SET reset_email_expire = ?, reset_email_code = ? WHERE user_uid = ?"
+        );
+        $stmt->bind_param("sss", $expireTime, $reset_email_code, $UserID);
+        $result = $stmt->execute();
+
+        if ($result){
+             $human_readable = $expirationTime->format('F j, Y \\a\\t g:i a'); 
+            $Body = "
+                       <!DOCTYPE html>
+       <body>
+       <h3>Email Reset Request:</h3>
+       <hr>
+       <br>
+       <p>
+       Here is your reset code: $reset_email_code
+       You will have 15 mins, until: $human_readable, to reset it, <br> afterward you will need to request a new code.
+       </p>
+       <p>Please go <a href='https://vehtrac.logandag.dev/ui/settings/res_email_step2.php?email=$UserInfoEmail'>Here</a> to finish the reset process.
+       If the link doesn't work, copy and paste this in your browser: <br> https://vehtrac.logandag.dev/ui/settings/res_email_step2.php?email=$UserInfoEmail</p>
+        </body>
+       </html>
+                ";
+            $mail->setFrom(
+                'no-reply@logandag.dev',
+                'VehTrac No reply'
+            );
+            $mail->addAddress($UserInfoEmail);
+            $mail->Subject = "VehTrac Email reset Verification";
+            $mail->Body = $Body;
+            
+            if (!$mail->send()){
+                $ErrorMessage[] = "There was an error submitting the request. Please contact support if issue persists. Error code: SETEMAILSEND1";
             }else{
-                $ErrorMessage[] = "We were unable to save your theme, please try again!";
+                $ErrorMessage[] = "Request submitted! Please finish the steps in the email, if you don't have it, please check your spam or resubmit the reset request!";
             }
-        }     
+        }else{
+            $ErrorMessage[] = "There was an error submitting the request. Please contact support if issue persists. Error code: SETEMAILRES2";
+        }
+
+    }else{
+        $ErrorMessage[] = "There was an error submitting the request. Please contact support if issue persists. Error code: SETEMAILRES1";
+    }
+}
+
+if (isset($_POST["submit_password"])){
+    
+}
 ?>
 <html>
-    <head>
-        <title>
-            VehTrac | Profile Settings
-        </title>
-    </head>
-<body data-bs-theme="<?php echo $_COOKIE['SiteTheme'] ?? 'light';?>">
-    <div class="main_site_content">
+
+<head>
+    <title>
+        VehTrac | Profile Settings
+    </title>
+</head>
+
+<body data-bs-theme="<?php echo $_COOKIE['SiteTheme'] ?? 'light'; ?>">
+    <div class="main_site_content h-100">
         <div class="text-center">
-        <h2 class="jumbotron">Account Settings:</h2>
+            <h2 class="jumbotron">Account Settings:</h2>
         </div>
 
         <a name="sessions"></a>
         <?php
-        require ("$DocRoot/BackPhp/DisplaySessions.php");
-          if (!isset($UserInfoUsername)){
+        require("$DocRoot/BackPhp/DisplaySessions.php");
+        if (!isset($UserInfoUsername)) {
             $UserInfoUsernameShow = "None set";
             $Disabled = "";
-          }else{
+        } else {
             $UserInfoUsernameShow = $UserInfoUsername;
             $Disabled = "disabled";
-          }
+        }
         ?>
         <hr>
-        
-         <div class="username_form ">
-            <form action="" method="post" class="d-flex flex-column align-items-center mt-3">
-                <div class="form-floating mb-3 w-auto">
-                <input type = "text" value = "<?php echo $UserInfoUsernameShow; ?>" id='username' name='username' <?php echo $Disabled; ?> class="form-control">
-                <label for="username">Username:</label>
-                <br>
-                <button class="w-auto btn btn-lg btn-primary mb-3" type="submit" name="submit_username" <?php echo $Disabled; ?>>Submit Username</button>
-                </div>
-            </form>
-            
-        </div>
-          <hr>
-        <div class="reset_password">
-
-        </div>
-        <hr>
-          <?php if(!empty($ErrorMessage)):?>
+        <?php if (!empty($ErrorMessage)): ?>
             <div class="alert alert-info error-container w-100 align-items-center text-center">
-                        <?php
-            $errors = implode("<br>", $ErrorMessage);
-            echo $errors;
-        ?>
+                <?php
+                $errors = implode("<br>", $ErrorMessage);
+                echo $errors;
+                ?>
             </div>
-            <?php endif;?>
+        <?php endif; ?>
+<div class="container">
+    <div class="row rows-cols-1 rows-col-sm-2 g-4">
+        <div class="col">
+            <div class="username_form">
+                <p class="fs-5 text-center">Reset Username:</p>
+                <form action="" method="post" class="d-flex flex-column align-items-center mt-3">
+                    <div class="form-floating mb-3 w-100">
+                        <input type="text" value="<?php echo $UserInfoUsernameShow; ?>" id='username' name='username' <?php echo $Disabled; ?> class="form-control">
+                        <label for="username">Username:</label>
+                        <br>
+                        <button class="w-100 btn btn-lg btn-primary mb-3" type="submit" name="submit_username" <?php echo $Disabled; ?>>Submit Username</button>
+                    </div>
+                </form>
+            </div>
+        </div>
 
+        <div class="col">
+            <div class="reset_password">
+                <p class="fs-5 text-center">Reset Password:</p>
+                <form action="" method="post" class="d-flex flex-column align-items-center mt-3">
+                    <div class="form-floating mb-3 w-100">
+                        <input type="password" id='reset_pass_new' name='reset_pass_new' class="form-control" <?php echo $PassDisable; ?>>
+                        <label for="reset_pass_new">New Password:</label>
+                    </div>
+                    <div class="form-floating mb-3 w-100">
+                        <input type="password" id='reset_pass_confirm' name='reset_pass_confirm' class="form-control" <?php echo $PassDisable; ?>>
+                        <label for="reset_pass_confirm">Confirm Password:</label>
+                        <br>
+                        <button class="w-100 btn btn-lg btn-primary mb-3" type="submit" name="submit_password" <?php echo $PassDisable; ?>>Reset Password</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+            <div class="col">
+                <div class="change_email">
+                  
+                <p class="fs-5 text-center">Change Email:</p>
+                <form action="" method="post" class="d-flex flex-column align-items-center mt-3">
+                    <div class="form-floating mb-3 w-100">
+                        <input type="email" id='new_email' name='new_email' class="form-control">
+                        <label for="new_email">New Email:</label>
+                    </div>
+                    <div class="form-floating mb-3 w-100">
+                        <input type="email" id='reset_email_confirm' name='reset_email_confirm' class="form-control">
+                        <label for="reset_email_confirm">Confirm New Email:</label>
+                        <br>
+                        <button class="w-100 btn btn-lg btn-primary mb-3" type="submit" name="submit_new_email">Start reset process</button>
+                    </div>
+                </form>
+            </div>
+            </div>
+            <div class="col">
         <div class="theme-switch d-flex flex-column align-items-center text-center">
-    <p class="fs-5">Display Theme:</p>
-    <span class="fs-6 text-muted">Choose a theme to sync across your account for devices.</span>
-<form class="d-flex flex-column align-items-center mt-3" action="" method="post">
+            <p class="fs-5">Display Theme:</p>
+            <span class="fs-6 text-muted">Choose a theme to sync across your account for devices.</span>
+            <form class="d-flex flex-column align-items-center mt-3" action="" method="post">
 
-    <!-- Light Theme Radio Option -->
-    <div class="form-check form-switch mb-2">
-        <input class="form-check-input" type="radio" id="theme_light" name="theme_choice" value="light" checked>
-        <label class="form-check-label" for="theme_light">Light theme.</label>
-    </div>
+                <!-- Light Theme Radio Option -->
+                <div class="form-check form-switch mb-2">
+                    <input class="form-check-input" type="radio" id="theme_light" name="theme_choice" value="light" checked>
+                    <label class="form-check-label" for="theme_light">Light theme.</label>
+                </div>
 
-    <!-- Dark Theme Radio Option -->
-    <div class="form-check form-switch mb-2">
-        <input class="form-check-input" type="radio" id="theme_dark" name="theme_choice" value="dark">
-        <label class="form-check-label" for="theme_dark">Dark theme.</label>
-    </div>
+                <!-- Dark Theme Radio Option -->
+                <div class="form-check form-switch mb-2">
+                    <input class="form-check-input" type="radio" id="theme_dark" name="theme_choice" value="dark">
+                    <label class="form-check-label" for="theme_dark">Dark theme.</label>
+                </div>
 
-    <button type="submit" class="btn btn-primary" name="theme_submit">Save theme choice</button>
-</form>
-
-</div>
+                <button type="submit" class="btn btn-primary" name="theme_submit">Save theme choice</button>
+             </form>
+        </div>
+        </div>
+        </div>
+        <hr>
+        <br />
+        <br />
+        </div>
+                <div class="text-center">
+            <h2 class="jumbotron">Profile Information:</h2>
+        </div>
+        <div class="container">
+            <div class="row rows-cols-1 rows-col-sm-2 g-4">
+             <div class="col">
+                <p class="fs-5 text-center">Current email: <?php echo $UserInfoEmail;?></p>
+                </div>
+            </div>
+        </div>
     </div> <!--END MAIN SITE CONTENT DIV-->
 </body>
 <?php require("$DocRoot/includes/footer.html"); ?>
+
 </html>
--- ui/settings/res_email_step2.php
+++ ui/settings/res_email_step2.php
@@ -12,56 +12,51 @@ $email = $_GET["email"];
 }else{
     $ErrorMessage[] = "Seems like the email is not in the URL. Please check the link again.";
 }
-if (isset($_GET["code"])){
-$reset_code = $_GET["code"];
-}else{
-    $ErrorMessage[] = "Seems like the Reset code is not in the URL. Please check the link again.";
-}
 
-if (isset($reset_code) && isset($email)){
-}
-if (isset($_POST["reset_password"])){
+if (isset($_POST["reset_email"])){
     if (isset($_SERVER['HTTP_CF_CONNECTING_IP'])){
         $IP = $_SERVER['HTTP_CF_CONNECTING_IP'];
     }else{
     $IP = $_SERVER["REMOTE_ADDR"];
     }
 
-        $json = file_get_contents("");
+        $json = file_get_contents("https://ipinfo.io/$IP/geo/?token=e99f8b9a79352f");
 $json1 = json_decode($json, true);
 $country = $json1['country'];
 $region = $json1['region'];
 $city = $json1['city'];
 $coordinates = $json1['loc'];
 $postal = $json1['postal'];
+    $old_email = $_POST["old_email"];
+    $new_email = $_POST["new_email"];
+    $conf_email = $_POST["conf_email"];
+    $reset_code_input = $_POST["conf_passcode"];
 
-    $new_password = $_POST["password_one"];
-    $conf_password = $_POST["conf_password"];
-    $entered_email = $_POST["reset_email"];
-
- if ($entered_email == $email){
-$stmt = $conn->prepare("SELECT email, reset_pass_code, reset_pass_time_expire FROM user_info WHERE email = ?");
-$stmt->bind_param("s", $entered_email);
+ if ($old_email == $email){
+$stmt = $conn->prepare("SELECT user_uid, reset_email_expire, reset_email_code FROM user_info WHERE email = ?");
+$stmt->bind_param("s", $old_email);
 $stmt->execute();
 $result = $stmt->get_result()->fetch_assoc();
+if (!$result){
+    $ErrorMessage[] = "That email does not seem to exist, please try the link again. If that doesn't work, please resubmit the reset request.";
+}else{
+$db_user_uid = $result["user_uid"];
+$db_reset_code = $result["reset_email_code"];
+$db_expire_time = $result["reset_email_expire"];
+        if ($reset_code_input == $db_reset_code){
 
-$db_reset_code = $result["reset_pass_code"];
-$db_expire_time = $result["reset_pass_time_expire"];
-        if ($reset_code == $db_reset_code){
-
-        if ($conf_password == $new_password){
-        $encrypted_pass = password_hash($conf_password, PASSWORD_BCRYPT);
+        if ($conf_email == $new_email){
         $GetcurrentTime = new DateTime();
         $currentTime = $GetcurrentTime->format('Y-m-d H:i:s');
 
         if ($currentTime <= $db_expire_time){
             $reset_success = "Y";
             $reset_code = 0;
-            $stmt = $conn->prepare("UPDATE user_info SET password = ?, reset_pass_code = ?, reset_success = ? WHERE email = ?");
-            $stmt->bind_param("ssss", $encrypted_pass, $reset_code, $reset_success, $email);
+            $stmt = $conn->prepare("UPDATE user_info SET email = ?, reset_email_code = ?, reset_email_success = ? WHERE user_uid = ?");
+            $stmt->bind_param("sssi", $new_email, $reset_code, $reset_success, $db_user_uid);
             $result = $stmt->execute();
             if($result){
-                $ErrorMessage[] = "Password reset was successful!";
+                $ErrorMessage[] = "Email reset was successful!";
                 $human_readable = $GetcurrentTime->format('F j, Y \\a\\t g:i a'); 
 
                                 $Body = "
@@ -71,14 +66,13 @@ $db_expire_time = $result["reset_pass_time_expire"];
        Security Alert.
        </h4>
        <p>
-       Your account recently had its password reset at: $human_readable. <br>
-       IP: $IP <br>
+       Your account recently had its email reset at: $human_readable. <br>
+       From IP: $IP <br>
        If this was you, there is no further action that needs to be taken and you can disregard this email. <br>
-       If you did not make these changes, please either click <a href='https://vehtrac.logandag.dev/ui/security'>Here.</a> <br>
-       Or, reset your passsword again and change your email and/or username information.
+       If you did not make these changes, please click <a href='https://vehtrac.logandag.dev/ui/support'>Here.</a> or contact support.<br>
        <br>
        <hr>
-       Location details of device who reset your password: <br>
+       Location details of device who reset your email: <br>
        $city, $region, $country
        <hr>
        <br>
@@ -93,23 +87,25 @@ $db_expire_time = $result["reset_pass_time_expire"];
                 'no-reply@logandag.dev',
                 'VehTrac No reply'
             );
-            $mail->addAddress($email);
-            $mail->Subject = "Password reset";
+            $mail->addAddress($old_email);
+            $mail->Subject = "Account Security Alert - Email was reset.";
             $mail->Body = $Body;
 
             if (!$mail->send()){}else{}
-            }else{$ErrorMessage[] = "Password could not reset!";}
+
+            }else{$ErrorMessage[] = "Email could not be reset! Error: EMAILRESST2UPDB";}
         }else{
             $ErrorMessage[] = "This code has expired, please request a new code.'";
         }
         }else{
-            $ErrorMessage[] = "Passwords do not match, please try again.";
+            $ErrorMessage[] = "Emails do not match, please try again.";
         }
     }else{
         $ErrorMessage[] = "The code in the URL and the one assigned do not match. Please check the link in the email or request a new code.";
-    }
-      }else{
-        $ErrorMessage[] = "The email you entered and the one for the code do not match. Please check the link in your email again or the email field.";
+        }
+    }      
+    }else{
+        $ErrorMessage[] = "The email in either the URL or the one given as the old email do not match, please check them again.";
     }
 }
 $conn->close();
@@ -117,7 +113,7 @@ $conn->close();
 <!DOCTYPE html>
 <html>
 <head>
-    <title>VehTrac | Password Reset step 2</title>
+    <title>VehTrac | Reset Email Step 2</title>
     </head>
 <body>
 
@@ -141,28 +137,34 @@ $conn->close();
         <form action="" method="post">
                                 <div class="form-floating mb-3">
                         <input type="email" class="form-control" 
-                               id="reset_email" name="reset_email" placeholder="name@example.com" required 
+                               id="old_email" name="old_email" placeholder="name@example.com" required 
                                value="">
-                        <label for="reset_email">Confirm email address:</label>
+                        <label for="old_email">Confirm old email address:</label>
                     </div>
                     <br>
                          <div class="form-floating mb-3">
-                        <input type="password" class="form-control" 
-                               id="password_one" name="password_one" placeholder="New Password:" required 
+                        <input type="email" class="form-control" 
+                               id="new_email" name="new_email" placeholder="New Email:" required 
                                value="">
-                        <label for="password_one">New Password:</label>
+                        <label for="new_email">New Email:</label>
                     </div>
                     <br>
                          <div class="form-floating mb-3">
-                        <input type="password" class="form-control" 
-                               id="conf_password" name="conf_password" placeholder="Confirm Password:" required 
+                        <input type="email" class="form-control" 
+                               id="conf_email" name="conf_email" placeholder="Confirm Email:" required 
                                value="">
-                        <label for="conf_password">Confirm Password:</label>
+                        <label for="conf_email">Confirm New Email:</label>
                     </div>
-
-                    <button class="w-100 btn btn-lg btn-primary mb-3" type="submit" name="reset_password">Reset Password</button>
-                     <a href="/resetpassword" class="btn btn-link btn-md text-decoration-none">Click for new code</a> <br>
-                     <a href="https://vehtrac.logandag.dev" class="btn btn-link btn-md text-decoration-none">Login</a>
+                    <br>
+                     <div class="form-floating mb-3">
+                        <input type="text" class="form-control" 
+                               id="conf_passcode" name="conf_passcode" placeholder="" required 
+                               value="">
+                        <label for="conf_passcode">Confirm Passcode:</label>
+                    </div>
+                    <br>
+                    <button class="w-100 btn btn-lg btn-primary mb-3" type="submit" name="reset_email">Reset Email</button>
+                    <p class="fs-5 alert alert-warning">If you need a new code, <a href="https://vehtrac.logandag.dev">signin</a> with your old info and go through the process again.</p>
         </form>
 
     </div>
